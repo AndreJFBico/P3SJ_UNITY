d47c0bae34786303ac168893430bce082a8c1fbf
ScientX <tori789@gmail.com>
2014-05-14 13:43:54 +0100

    fixes

--- Assets/Resources/Scripts/EnemyAgent.cs
+++ Assets/Resources/Scripts/EnemyAgent.cs
@@ -42,7 +42,7 @@ public class EnemyAgent : MonoBehaviour, Enemy
             if (firing)
             {
                 GameObject bullet = Instantiate(prefab, transform.position + 2 * transform.forward, transform.rotation) as GameObject;
-                bullet.rigidbody.AddForce(transform.forward * 1000);
+                bullet.rigidbody.AddForce(((player.position + new Vector3(0f, 2f, 0f)) - transform.position).normalized * 1300);
             }
             yield return new WaitForSeconds(0.5f);
         }
